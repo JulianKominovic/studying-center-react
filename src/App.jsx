01b96d30497611eb4ba629c8f21dc1ba86bfbe7f
Julian Kominovic <juliankominovic@gmail.com>
2021-06-27 19:26:46 -0300

    primer proyecto react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Fragment } from "react";
+import { Link, Route } from "wouter";
+import Home from "./pages/Home";
+import "./globals.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>ok</p>
+    <div className="wrapper">
+      <Home />
     </div>
   );
 }
---
+++ src/components/blurry-bg/BlurryBackground.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment } from "react";
+import "./BlurryBackground.css";
+
+const BlurryBackground = (props) => {
+  const { imgBackgroundUrl, imgCircleUrl } = props;
+
+  console.log(props);
+
+  const defaultCircle = () => {
+    return (
+      <svg className="background__ball">
+        <defs>
+          <radialGradient id="MyGradient">
+            <stop offset="0%" stopColor="#FF0099" />
+            <stop offset="100%" stopColor="rgba(255,61,0,.83)" />
+          </radialGradient>
+        </defs>
+        <circle className="ball-color" cx="50%" cy="50%" r="30%" />
+      </svg>
+    );
+  };
+
+  const imageCircle = () => {
+    return <img className="background__circle-img" src={imgCircleUrl}></img>;
+  };
+
+  return (
+    <Fragment>
+      <div className="background__blur"></div>
+      {console.log(typeof imgCircleUrl)}
+      {typeof imgCircleUrl === typeof "" ? imageCircle() : defaultCircle()}
+      <img src={imgBackgroundUrl} className="background__img"></img>
+    </Fragment>
+  );
+};
+
+export default BlurryBackground;
